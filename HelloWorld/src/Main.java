7803825fb3e9f5af807299076d30ad4e59f85aa9
Tóth Beáta Georgina <tothbeatageorgina@gmail.com>
2025-03-15 19:32:44 +0100

    saját gépről

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("HELLO WORLD!!!!");
 		System.out.println("Hátha most menni fooogg végre!!!!");
+		System.out.println("Most ez a saját gépemről van");
 	}
 
 }
